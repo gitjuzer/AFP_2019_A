27a04683a4b1a5d84ab6633c47d4fefe35146a83
csutorasd <43805183+csutorasd@users.noreply.github.com>
2019-03-11 08:41:53 +0100

    Backend - Adatbázis modul hozáadása

--- Backend/DatabaseConnection.php
+++ Backend/DatabaseConnection.php
@@ -1,6 +1,32 @@
 <?php
 
 class DatabaseConnection {
-    
+
+    $server;
+    $username;
+    $password;
+    $db;
+
+    $connection;
+
+    public function getConnection() {
+        $con = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname) or die("Connection failed: " . mysqli_connect_error());
+
+        if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_error());
+            exit();
+        } else {
+            $this->connection = $con;
+        }
+        
+        return $this->connection;
+    }
+
+    public function __construct() {
+        $this->server = SERVERNAME;
+        $this->username = USERNAME;
+        $this->password = PASSWORD;
+        $this->db = DBNAME;
+    }
 }
 
