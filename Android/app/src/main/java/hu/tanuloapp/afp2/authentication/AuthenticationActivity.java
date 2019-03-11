e325a8e72c1cbfc97ead570b26c037e15cdc41ba
Tóth Kristóf <tothkrisi@gmail.com>
2019-03-11 09:24:57 +0100

    Android - AuthenticationActivity hozzáadása

--- Android/app/src/main/java/hu/tanuloapp/afp2/authentication/AuthenticationActivity.java
+++ Android/app/src/main/java/hu/tanuloapp/afp2/authentication/AuthenticationActivity.java
@@ -1,7 +1,11 @@
 package hu.tanuloapp.afp2.authentication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import hu.tanuloapp.afp2.R;
+import hu.tanuloapp.afp2.authentication.adapter.FragmentAdapter;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class AuthenticationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        if (viewPager != null) {
+            FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
+            viewPager.setAdapter(adapter);
+        }
+
     }
 }
