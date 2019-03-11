86a3ba0dd3e4e9f676eb4a5a9b882dc7882790e4
csutorasd <43805183+csutorasd@users.noreply.github.com>
2019-03-11 09:00:49 +0100

    Backend - Adatbázis modul Hibajavítás

--- Backend/index.php
+++ Backend/index.php
@@ -1,2 +1,23 @@
 <?php
+require_once('./config.php');
+require_once('./DatabaseConnection.php');
+$db = new DatabaseConnection();
+$connection = $db.getConnection();
+$request_method = $_SERVER['REQUEST_METHOD'];
+
+
+switch($request_method) {
+    case 'GET':
+        break;
+    case 'POST':
+        break;
+    case 'PUT':
+        break;
+    case 'DELETE':
+        break;
+    default:
+        header("HTTP/1.0 405 Method Not Allowed");
+        echo 'Invalid Request method.';
+        break;
+}
 
