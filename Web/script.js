910aaa596973903a853dad0f2a046684840304c2
vrblymt <43416037+vrblymt@users.noreply.github.com>
2019-03-13 15:15:52 +0100

    Regisztráció ellenőrzés
    ellenőrzi a felhazsnálónév hosszát, a beírt jelszó hosszát és annak egyezését

--- Web/script.js
+++ Web/script.js
@@ -4,3 +4,22 @@ function toRegisterPage() {
 function toHomepage() {
   location.href = "Homepage.html";
 }
+function validateRegister() {
+  var actualUsername = $("#username").val();
+  var actualPasswd = $("#password").val();
+  var actualPasswd1x = $("#password1x").val();
+  var registerState = $("#registerState");
+  if (actualUsername.length < 6) {
+    registerState.css("color", "red");
+    registerState.html("A beírt név nem lehet 6 karakternél rövidebb.");
+  } else if (actualPasswd.length < 6 || actualPasswd1x.length < 6) {
+    registerState.css("color", "red");
+    registerState.html("A beírt jelszó nem lehet 6 karakternél rövidebb.");
+  } else if (actualPasswd != actualPasswd1x) {
+    registerState.css("color", "red");
+    registerState.html("A beírt jelszó nem egyezik.");
+  } else {
+    registerState.css("color", "green");
+    registerState.html("Regisztráció sikeres.");
+  }
+}
