e84c58de26d1ac0ea7fb7989965dad2d3281f6ff
Bálint Bogáti <balint1994@gmail.com>
2019-03-18 09:32:31 +0100

    Android - Tasks package létrehozva

--- Android/app/src/main/java/hu/tanuloapp/afp2/QuizActivity.java
+++ Android/app/src/main/java/hu/tanuloapp/afp2/QuizActivity.java
@@ -8,6 +8,6 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
+        setContentView(R.layout.fragment_quiz);
     }
 }
