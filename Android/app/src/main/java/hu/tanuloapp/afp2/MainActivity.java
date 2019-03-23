34455b48f20a516924bd3401694d7a791d60eca2
Bálint Bogáti <balint1994@gmail.com>
2019-03-23 12:32:40 +0100

    Android - QuizActivity átnevezése MainActivity-re

--- Android/app/src/main/java/hu/tanuloapp/afp2/MainActivity.java
+++ Android/app/src/main/java/hu/tanuloapp/afp2/MainActivity.java
@@ -3,7 +3,7 @@ package hu.tanuloapp.afp2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class QuizActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
