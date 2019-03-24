49bff3ecfb63e8323ad5198ef6aee9d3e076c993
Tóth Kristóf <tothkrisi@gmail.com>
2019-03-25 00:17:09 +0100

    Android - Regisztrációs felület összekötése

--- Android/app/src/main/java/hu/tanuloapp/afp2/authentication/fragments/RegistrationFragment.java
+++ Android/app/src/main/java/hu/tanuloapp/afp2/authentication/fragments/RegistrationFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import hu.tanuloapp.afp2.R;
 
@@ -24,7 +26,22 @@ public class RegistrationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false);
+        View view = inflater.inflate(R.layout.fragment_registration, container, false);
+
+        EditText username = view.findViewById(R.id.Name);
+        EditText password = view.findViewById(R.id.Password);
+        EditText password2 = view.findViewById(R.id.Password2);
+
+        Button login = view.findViewById(R.id.Login);
+        login.setOnClickListener(v -> {
+            String user = username.getText().toString();
+            String pass = password.getText().toString();
+            String pass2 = password2.getText().toString();
+            // TODO: 2019.03.24. login business logic
+            
+        });
+
+        return view;
     }
 
 }
