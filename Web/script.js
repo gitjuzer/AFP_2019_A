91b3c5e4f495a45c644d5377da852cc448fe767a
vrblymt <43416037+vrblymt@users.noreply.github.com>
2019-03-25 09:26:00 +0100

    Regisztrációs adatok küldése szerverre

--- Web/script.js
+++ Web/script.js
@@ -19,10 +19,19 @@ function validateRegister() {
     registerState.css("color", "red");
     registerState.html("A beírt jelszó nem egyezik.");
   } else {
-    registerState.css("color", "green");
-    registerState.html("Regisztráció sikeres.");
+    var data = JSON.parse({"username" : actualUsername,"password": actualPasswd,"email":""});
+    $.ajax({
+      type: "POST",
+      url: "localhost/afp/index.php",
+      data: data,
+      success: function() { registerState.css("color", "green");
+                            registerState.html("Regisztráció sikeres.");},
+      dataType: "json"
+    });
+    
   }
 }
+
 
 function validateLogin() {
   var loginUsername = $("#loginUsername").val();
