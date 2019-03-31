8565e706df0d388bbfcbb065c13675ebb003efbf
Bálint Bogáti <balint1994@gmail.com>
2019-03-31 12:21:21 +0200

    Android - SyllabusFragment hozzáadása

---
+++ Android/app/src/main/java/hu/tanuloapp/afp2/tasks/fragments/SyllabusFragment.java
@@ -0,0 +1,19 @@
+package hu.tanuloapp.afp2.tasks.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SyllabusFragment extends Fragment {
+
+    public SyllabusFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
