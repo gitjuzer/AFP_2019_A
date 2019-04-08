067da57d67b11d2895554069456bafd9c110d405
Tóth Kristóf <tothkrisi@gmail.com>
2019-04-08 09:27:13 +0200

    Android - Profil fragment átmozgatása

---
+++ Android/app/src/main/java/hu/tanuloapp/afp2/profil/ProfileFragment.java
@@ -0,0 +1,19 @@
+package hu.tanuloapp.afp2.profil;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
