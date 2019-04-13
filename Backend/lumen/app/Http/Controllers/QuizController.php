4597ba3248f9a04309280b28b9d0d6b9989cf434
csutorasd <43805183+csutorasd@users.noreply.github.com>
2019-04-13 08:37:35 +0200

    Backend - Kérdések, válaszok lekérdezése

--- Backend/lumen/app/Http/Controllers/QuizController.php
+++ Backend/lumen/app/Http/Controllers/QuizController.php
@@ -8,5 +8,15 @@ class QuizController extends Controller {
         return response()->json(app('db')->select('SELECT * FROM test'));
     }
 
+    public function getQuizById($quiz) {
+        $questions = app('db')->select('SELECT * FROM question WHERE test=?', [$quiz]);
+        $length = count($questions);
 
+        for ($i = 0; $i < $length; $i++) {
+            $answers = app('db')->select('SELECT * FROM answer WHERE question=?', [$questions[$i]->id]);
+            $questions[$i]->answers = $answers;
+        }
+
+        return response()->json($questions);
+    }
 }
