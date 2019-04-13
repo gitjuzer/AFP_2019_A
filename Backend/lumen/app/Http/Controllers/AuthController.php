35ae350ddb7d90f352181f491cbe138e4da6c637
csutorasd <43805183+csutorasd@users.noreply.github.com>
2019-04-13 08:28:58 +0200

    Backend - Login hozzáadása

--- Backend/lumen/app/Http/Controllers/AuthController.php
+++ Backend/lumen/app/Http/Controllers/AuthController.php
@@ -31,4 +31,17 @@ class AuthController extends Controller {
         }
     }
 
+    public function login(Request $request) {
+        $user = $request->input('user');
+        $password = $request->input('password');
+
+        $result = app('db')->select('SELECT token FROM user WHERE username=? AND password=?', [$user, $password]);
+
+        if ($result != null && !empty($result)) {
+            return response()->json(['status' => 1, 'status_message' => $result[0]]);
+        } else {
+            return response()->json(['status' => 0, 'status_message' => 'Login failed.']);
+        }
+    }
+
 }
