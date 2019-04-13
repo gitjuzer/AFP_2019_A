631f4d8975ac2c0e56f02cda305d508c6178b110
csutorasd <43805183+csutorasd@users.noreply.github.com>
2019-04-13 13:48:48 +0200

    Backend - Kérdés feltöltése Hozzáadás

--- Backend/lumen/app/Http/Controllers/QuizManageController.php
+++ Backend/lumen/app/Http/Controllers/QuizManageController.php
@@ -13,4 +13,11 @@ class QuizManageController extends Controller {
         return response()->json($insertedId);
     }
 
+    public function insertQuestion(Request $request) {
+        $question = $request->input('question');
+        $test = $request->input('quiz');
+
+        $insertedId = app('db')->table('question')->insertGetId(['question' => $question, 'test' => $test, 'type' => 1]);
+        return response()->json($insertedId);
+    }
 }
