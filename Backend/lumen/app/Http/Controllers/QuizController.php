fc16d1689fead06e60a1018af0c04c3667f91ef6
csutorasd <43805183+csutorasd@users.noreply.github.com>
2019-04-13 08:36:59 +0200

    Backend - Összes Quiz lekérdezése

--- Backend/lumen/app/Http/Controllers/QuizController.php
+++ Backend/lumen/app/Http/Controllers/QuizController.php
@@ -4,4 +4,9 @@ namespace App\Http\Controllers;
 
 class QuizController extends Controller {
 
+    public function getAllQuiz() {
+        return response()->json(app('db')->select('SELECT * FROM test'));
+    }
+
+
 }
