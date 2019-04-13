a1981e16feb24bad340f294f445fa2587da8f05f
csutorasd <43805183+csutorasd@users.noreply.github.com>
2019-04-13 08:41:26 +0200

    Backend - Toplista, Adatok beolvasása

--- Backend/lumen/app/Http/Controllers/ToplistController.php
+++ Backend/lumen/app/Http/Controllers/ToplistController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class ToplistController extends Controller {
 
-    
+    public function insertScore(Request $request) {
+        $score = $request->input('score');
+        $quiz = $request->input('quiz_id');
+        $user = $request->input('user_id');
+
+        
+    }
 
 }
