37c38c272003f5eb8ab6bf1cfca8e07781c083ed
csutorasd <43805183+csutorasd@users.noreply.github.com>
2019-04-13 08:42:03 +0200

    Backend - Elért pontszám hozzáadása Adatbázishoz

--- Backend/lumen/app/Http/Controllers/ToplistController.php
+++ Backend/lumen/app/Http/Controllers/ToplistController.php
@@ -11,7 +11,13 @@ class ToplistController extends Controller {
         $quiz = $request->input('quiz_id');
         $user = $request->input('user_id');
 
-        
+        $result = app('db')->insert('INSERT INTO score (score, test_id, user_id) VALUES (?, ?, ?)', [$score, $quiz, $user]);
+
+        if ($result) {
+            return response()->json(['status' => 1, 'status_message' => 'Score submitted.']);
+        } else {
+            return response()->json(['status' => 0, 'status_message' => 'Score submit failed.']);
+        }
     }
 
 }
