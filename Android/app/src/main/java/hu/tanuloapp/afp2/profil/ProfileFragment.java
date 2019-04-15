11e8fdd52d22622cea0beb72eb77ccf1160cb941
NagyAkos <43416062+NagyAkos@users.noreply.github.com>
2019-04-15 09:20:46 +0200

    Android - felület összekötése az üzleti logikával

--- Android/app/src/main/java/hu/tanuloapp/afp2/profil/ProfileFragment.java
+++ Android/app/src/main/java/hu/tanuloapp/afp2/profil/ProfileFragment.java
@@ -1,19 +1,42 @@
 package hu.tanuloapp.afp2.profil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import hu.tanuloapp.afp2.R;
+import hu.tanuloapp.afp2.authentication.AuthenticationActivity;
+import hu.tanuloapp.afp2.authentication.fragments.LoginFragment;
 
 public class ProfileFragment extends Fragment {
+
 
     public ProfileFragment() {
 
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profil, container, false);
+        Button logout=view.findViewById(R.id.logout);
+        logout.setOnClickListener(v -> {
+           // getFragmentManager().beginTransaction().replace(R.id.mainFrame,new );
+           startActivity(new Intent(getContext(), AuthenticationActivity.class));
+
+        });
+        Button passwordchange=view.findViewById(R.id.passwordchange);
+        passwordchange.setOnClickListener(v -> {
+            getFragmentManager().beginTransaction().replace(R.id.mainFrame,new PasswordChange() ).commit();
+            //startActivity(new Intent(getContext(), AuthenticationActivity.class));
+
+        });
+
+        return view;
+
     }
 }
