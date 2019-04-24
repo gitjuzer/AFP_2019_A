6742b0577e107042ae60ec135757a693c7275381
csutorasd <43805183+csutorasd@users.noreply.github.com>
2019-04-24 18:44:44 +0200

    Backend - Cors Header hozzáadása minden kéréshez

--- Backend/lumen/app/Http/Controllers/AuthController.php
+++ Backend/lumen/app/Http/Controllers/AuthController.php
@@ -8,14 +8,12 @@ class AuthController extends Controller {
 
     public function register(Request $request) {
         $user = $request->input('user');
-        header("Access-Control-Allow-Origin: *");
 
         if (!$this->checkIfUserExists($user)) {
             $password = $request->input('password');
             $email = $request->input('email');
             $token = uniqid('');
 
-            //$result = app('db')->insert('INSERT INTO `user`(`username`, `password`, `email`, `token`) VALUES (?,?,?,?)', [$user, $password, $email, $token]);
             $insertedId = app('db')->table('user')->insertGetId(['username' => $user, 'password' => $password, 'email' => $email, 'token' => $token]);
             app('db')->insert('INSERT INTO user_role VALUES (?,?)', [$insertedId, 1]);
 
@@ -40,8 +38,6 @@ class AuthController extends Controller {
 
         $result = app('db')->select('SELECT token, name AS role FROM user INNER JOIN user_role ON user.id = user_role.user_id INNER JOIN role ON role.id = user_role.role_id WHERE username=? AND password=?', [$user, $password]);
 
-
-        header("Access-Control-Allow-Origin: *");
         if ($result != null && !empty($result)) {
             return response()->json(['status' => 1, 'status_message' => $result[0]]);
         } else {
