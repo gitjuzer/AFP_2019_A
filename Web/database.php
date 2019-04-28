05b58412257b59a7820cac7e8f12b75b0e4d5e39
gdavid06 <43856829+gdavid06@users.noreply.github.com>
2019-04-28 18:09:14 +0200

    kérdés felvitele php-ban

--- Web/database.php
+++ Web/database.php
@@ -13,4 +13,17 @@
 		
 		return $conn;
 	}
+	
+	function insertQuestion($question,$answer1,$answer2,$answer3,$answer4) {
+		$conn = getConnection();
+		//a mező neveit át kell írni!!
+		$sql = "INSERT INTO Questions (question, 1st_answer, 2nd_answer,3rd_answer,4th_answer)
+		VALUES (".$question.",".$answer1.",".$answer2.",".$answer3",".$answer4")";
+
+		if ($conn->query($sql) === TRUE) {
+			echo "Successful insert!";
+
+
+		$conn->close();
+	}
 ?> 
