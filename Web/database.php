d6b42345d7c86be0afadeb8deb218674126ad6fa
gdavid06 <43856829+gdavid06@users.noreply.github.com>
2019-04-29 09:27:50 +0200

    database.php szintaktikai hibák javítása

--- Web/database.php
+++ Web/database.php
@@ -4,24 +4,24 @@
 		//az adatbázis adatait át kell írni
 		$DB_TYPE = 'mysql';
 		$DB_HOST = 'localhost';
-		$DB_NAME = '';
+		$DB_NAME = 'quiz';
 		$USER = 'root';
 		$PASS = '';
 		
 		$connection = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME.';',USER, PASS);
 		$connection->exec("SET NAMES 'utf8'");
+		echo 'Successful connection';
 		return $connection;
 	}
 	
 	function insertQuestion($question,$answer1,$answer2,$answer3,$answer4) {
 		$conn = getConnection();
 		//a mező neveit át kell írni!!
-		$sql = "INSERT INTO Questions (question, 1st_answer, 2nd_answer,3rd_answer,4th_answer)
-		VALUES (".$question.",".$answer1.",".$answer2.",".$answer3",".$answer4")";
+		$sql = 'INSERT INTO Questions (question, 1st_answer, 2nd_answer,3rd_answer,4th_answer) VALUES ('.$question.','.$answer1.','.$answer2.','.$answer3.','.$answer4.')';
 
 		if ($conn->query($sql) === TRUE) {
 			echo "Successful insert!";
-
+		}
 
 		$conn->close();
 	}
@@ -37,5 +37,4 @@
 		
 		return $result;
 	}
-	
-?> 
+?>
