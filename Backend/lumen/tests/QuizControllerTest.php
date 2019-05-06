44a7ae873858b38a2b44b3965e91290ccf2eac98
csutorasd <43805183+csutorasd@users.noreply.github.com>
2019-05-06 08:24:58 +0200

    Backend - Quiz lekérdezése id szerint Teszt

--- Backend/lumen/tests/QuizControllerTest.php
+++ Backend/lumen/tests/QuizControllerTest.php
@@ -7,9 +7,14 @@ class ExampleTest extends TestCase
 {
 
     public function testGetAllQuiz() {
-        $response = $this->call('GET', 'quiz');
+        $response = $this->call('GET', '/quiz');
 
         $this->assertEquals(200, $response->status());
     }
 
+    public function testGetQuizById() {
+        $response = $this->call('GET', '/quiz', ['id' => 1]);
+
+        $this->assertEquals(200, $response->status());
+    }
 }
