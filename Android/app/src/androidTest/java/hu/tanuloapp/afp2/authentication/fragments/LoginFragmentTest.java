43541c0170118ccd73df1de0203c3ad8c82e5e47
alatriste5 <balazs799@gmail.com>
2019-06-07 11:58:38 +0200

    Android - Tesztelendő funkciók
    Dokumentáció hozzáadása, kiegészítése

---
+++ Android/app/src/androidTest/java/hu/tanuloapp/afp2/authentication/fragments/LoginFragmentTest.java
@@ -0,0 +1,18 @@
+package hu.tanuloapp.afp2.authentication.fragments;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginFragmentTest {
+
+    @Test
+    public void login() {
+        String username = "balazs799";
+        String password = "balazs5";
+
+        LoginFragment bej = new LoginFragment();
+
+
+    }
+}
